aea160c6ca6b61606882fb3f99943becc44f7eea
FladeX <FladeX@yandex.ru>
2014-08-09 22:00:40 +0400

    add some scripts

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var Site = {
+    Models: {},
+    Views: {},
+    Collections: {}
+};
+Site.Models.Chapter = Backbone.Model.extend({
+
+});
+Site.Views.ChapterView = Backbone.View.extend({
+    tagName: 'div',
+    template: $('#chapterTemplate').html(),
+
+    events: {
+        'click .chapter__controls-icon_mode_add': 'addChapter',
+        'click .chapter__controls-icon_mode_edit': 'editChapter',
+        'click .chapter__controls-icon_mode_delete': 'deleteChapter',
+        'click .chapter__editor-submit': 'saveChapter'
+    },
+
+    initialize: function() {
+        this.render();
+    },
+
+    render: function() {
+        var tmpl = _.template(this.template);
+
+        this.$el.html(tmpl(this.model.toJSON()));
+        return $this;
+    },
+
+    addChapter: function() {
+        console.log('add chapter');
+    },
+    editChapter: function() {
+        console.log('edit chapter');
+    },
+    deleteChapter: function() {
+        console.log('delete chapter');
+    },
+    saveChapter: function() {
+        console.log('save chapter');
+    }
+});
+
+
+$(document).ready(function(){
+})
